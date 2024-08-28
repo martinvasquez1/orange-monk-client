3e8f17292a0ff3e00a32dca0a567748376805275
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2024-08-27 21:12:00 -0400

    Update router to component like config

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,4 +1,6 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import AuthRoute from './components/AuthRoute.jsx';
 
 import Layout from './components/Layout.jsx';
 import Landing from './pages/Landing.jsx';
@@ -10,23 +12,26 @@ import AppLayout from './components/AppLayout.jsx';
 import Home from './pages/Home.jsx';
 
 export default function Router() {
-  const router = createBrowserRouter([
-    {
-      path: '/app',
-      element: <AppLayout />,
-      children: [{ index: true, element: <Home /> }],
-    },
-    {
-      path: '/',
-      element: <Layout />,
-      children: [
-        { index: true, element: <Landing /> },
-        { path: 'about', element: <About /> },
-        { path: '/sign-up', element: <SignUp /> },
-        { path: '/sign-in', element: <SignIn /> },
-      ],
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/app"
+          element={
+            <AuthRoute>
+              <AppLayout />
+            </AuthRoute>
+          }
+        >
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Landing />} />
+          <Route path="about" element={<About />} />
+          <Route path="sign-up" element={<SignUp />} />
+          <Route path="sign-in" element={<SignIn />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
