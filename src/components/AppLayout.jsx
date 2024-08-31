7c378f28e36e1ec5db0f717a6fef02710213890c
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2024-08-31 16:41:34 -0400

    Add AppNavbar

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,12 +1,19 @@
 import { Outlet } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
+import AppNavbar from './AppNavbar';
+
 export default function AppLayout({}) {
   return (
     <div>
-      <p>The app Layout</p>
-      <Link to="#">?</Link>
-      <Outlet />
+      <AppNavbar />
+      <div className="flex">
+        <div className="flex-1 bg-red-200">Hi</div>
+        <div className="flex-[3_3_0%] bg-green-200">
+          <Outlet />
+        </div>
+        <div className="flex-1 bg-blue-200">Hi</div>
+      </div>
     </div>
   );
 }
