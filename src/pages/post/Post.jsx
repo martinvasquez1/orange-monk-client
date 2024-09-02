65dc1f0053574344cd09f7ab7909d9568f6a496f
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2024-09-02 19:11:37 -0400

    Add Comments

--- src/pages/post/Post.jsx
+++ src/pages/post/Post.jsx
@@ -1,6 +1,7 @@
 import Icon from '../../components/Icon';
 import { GoComment } from 'react-icons/go';
 import GroupAside from '../group/GroupAside';
+import CommentsSection from './CommentsSection';
 
 const data = {
   id: 1,
@@ -15,24 +16,29 @@ export default function Post() {
   return (
     <div className="flex gap-4 my-6">
       <div className="rounded-2xl py-2 px-4 block flex-[3_3_0%]">
-        <div className="flex gap-4 items-center">
-          <img
-            src="https://images.unsplash.com/photo-1485846234645-a62644f84728?q=80&w=1459&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Profile picture."
-            className="h-10 w-10 rounded-full"
-          />
-          <div className="text-lg">{data.user}</div>
-        </div>
-        <div className="mt-3">
-          <h2 className="font-bold text-2xl">{data.title}</h2>
-          <h2 className="mt-2 text-lg">{data.body}</h2>
-        </div>
-        <div className="mt-4 hover:bg-slate-200 inline-block px-4 py-2 bg-slate-100 rounded-xl">
-          <div className="flex gap-2">
-            <Icon icon={<GoComment />} />
-            <span>{data.amountComments}</span>
+        <div>
+          {' '}
+          {/* Post */}
+          <div className="flex gap-4 items-center">
+            <img
+              src="https://images.unsplash.com/photo-1485846234645-a62644f84728?q=80&w=1459&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Profile picture."
+              className="h-10 w-10 rounded-full"
+            />
+            <div className="text-lg">{data.user}</div>
+          </div>
+          <div className="mt-3">
+            <h2 className="font-bold text-2xl">{data.title}</h2>
+            <h2 className="mt-2 text-lg">{data.body}</h2>
+          </div>
+          <div className="mt-4 hover:bg-slate-200 inline-block px-4 py-2 bg-slate-100 rounded-xl">
+            <div className="flex gap-2">
+              <Icon icon={<GoComment />} />
+              <span>{data.amountComments}</span>
+            </div>
           </div>
         </div>
+        <CommentsSection />
       </div>
       <div className="flex-1">
         <GroupAside />
