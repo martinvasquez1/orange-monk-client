96dc351cf9f66cc0b697bada23ffe358bd338f25
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2024-09-03 18:14:18 -0400

    Add create post button

--- src/pages/group/Group.jsx
+++ src/pages/group/Group.jsx
@@ -1,6 +1,7 @@
 import GroupOverview from './GroupOverview';
 import GroupFeed from './GroupFeed';
 import GroupAside from './GroupAside';
+import CreatePostButton from './CreatePostButton';
 
 export default function Group() {
   return (
@@ -8,6 +9,7 @@ export default function Group() {
       <GroupOverview />
       <div className="flex gap-4 my-6">
         <div className="flex-[3_3_0%]">
+          <CreatePostButton />
           <GroupFeed />
         </div>
         <div className="flex-1">
