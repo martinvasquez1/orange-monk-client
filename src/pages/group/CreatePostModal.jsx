3c2108552f48180aaad3ac885f445961e5a33378
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2024-09-03 19:44:14 -0400

    Add post modal

---
+++ src/pages/group/CreatePostModal.jsx
@@ -0,0 +1,55 @@
+export default function CreatePostModal() {
+  const id = 'create_post_modal';
+
+  return (
+    <dialog id={id} className="modal modal-bottom sm:modal-middle">
+      <div className="modal-box sm:w-2/3 sm:max-w-[600px]">
+        <form method="dialog">
+          {/* if there is a button in form, it will close the modal */}
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            ✕
+          </button>
+        </form>
+
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            document.getElementById(id).close();
+          }}
+        >
+          <h3 className="text-xl font-bold">Create Post</h3>
+          <label className="form-control w-full max-w-xs mt-2">
+            <div className="label">
+              <span className="label-text">Title</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Lorem"
+              className="input input-bordered w-full max-w-xs"
+              required
+            />
+          </label>
+          <label className="form-control mt-2">
+            <div className="label">
+              <span className="label-text">Body</span>
+            </div>
+            <textarea
+              className="textarea textarea-bordered h-24"
+              placeholder="Sed ut perspiciatis unde omnis iste natus error sit..."
+              required
+            ></textarea>
+          </label>
+
+          <div className="flex justify-end mt-6">
+            <button type="submit" className="btn">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+      <form method="dialog" className="modal-backdrop">
+        <button>close</button>
+      </form>
+    </dialog>
+  );
+}
