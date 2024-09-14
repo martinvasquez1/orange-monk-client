1d4fd3821c40b1d54144c3028f59cf67f348d232
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2024-09-14 19:37:48 -0300

    Add create group functionality

--- src/api/groups.js
+++ src/api/groups.js
@@ -3,3 +3,9 @@ import axiosInstance from './axiosInstance';
 export function getGroups() {
   return axiosInstance.get(`/groups`).then((res) => res.data);
 }
+
+export function createGroup({ name, description, privacy }) {
+  return axiosInstance
+    .post(`/groups`, { name, description, privacy })
+    .then((res) => res.data);
+}
--- src/pages/CreateGroup.jsx
+++ src/pages/CreateGroup.jsx
@@ -1,9 +1,32 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
+import { createGroup } from './../api/groups';
+
 export default function CreateGroup() {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [isPrivate, setIsPrivate] = useState(false);
+
+  const navigate = useNavigate();
+
+  const mutation = useMutation({
+    mutationFn: createGroup,
+    onSuccess: () => {
+      navigate('/app');
+    },
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    mutation.mutate({ name, description, isPrivate });
+  }
+
   return (
     <div className="flex justify-center">
       <div className="w-full max-w-3xl rounded-2xl bg-base-100 p-8 shadow">
         <div className="text-2xl font-bold">Create group</div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label className="form-control mt-2 w-full max-w-xs">
             <div className="label">
               <span className="label-text">Name</span>
@@ -13,6 +36,7 @@ export default function CreateGroup() {
               placeholder="Lorem"
               className="input input-bordered w-full max-w-xs"
               required
+              onChange={(e) => setName(e.target.value)}
             />
           </label>
           <label className="form-control mt-2">
@@ -23,19 +47,9 @@ export default function CreateGroup() {
               className="textarea textarea-bordered h-24"
               placeholder="Sed ut perspiciatis unde omnis iste natus error sit..."
               required
+              onChange={(e) => setDescription(e.target.value)}
             ></textarea>
           </label>
-          <label className="form-control mt-2">
-            <div className="label">
-              <span className="label-text">Rules</span>
-            </div>
-            <textarea
-              className="textarea textarea-bordered h-24"
-              placeholder="Sed ut perspiciatis unde omnis iste natus error sit..."
-              required
-            ></textarea>
-          </label>
-
           <div className="form-control mt-2">
             <div className="label">
               <span className="label-text">Visibility</span>
@@ -50,7 +64,7 @@ export default function CreateGroup() {
                   type="radio"
                   name="radio-10"
                   className="radio checked:bg-primary"
-                  defaultChecked
+                  onChange={(e) => setIsPrivate(false)}
                 />
               </label>
             </div>
@@ -61,31 +75,11 @@ export default function CreateGroup() {
                   type="radio"
                   name="radio-10"
                   className="radio checked:bg-primary"
-                  defaultChecked
+                  onChange={(e) => setIsPrivate(true)}
                 />
               </label>
             </div>
           </div>
-
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text">Logo</span>
-            </div>
-            <input
-              type="file"
-              className="file-input file-input-bordered w-full max-w-xs"
-            />
-          </label>
-
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text">Banner</span>
-            </div>
-            <input
-              type="file"
-              className="file-input file-input-bordered w-full max-w-xs"
-            />
-          </label>
 
           <div className="flex justify-end">
             <button type="submit" className="btn btn-primary mt-4">
