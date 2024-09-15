b96ff6cd229d26d7942520348d2451f3eaaf821a
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2024-09-15 18:20:45 -0300

    Add fetch to group overview

--- src/api/groups.js
+++ src/api/groups.js
@@ -4,6 +4,10 @@ export function getGroups() {
   return axiosInstance.get(`/groups`).then((res) => res.data);
 }
 
+export function getGroup(id) {
+  return axiosInstance.get(`/groups/${id}`).then((res) => res.data);
+}
+
 export function createGroup({ name, description, isPrivate, owner }) {
   return axiosInstance
     .post(`/groups`, { name, description, private: isPrivate, owner })
--- src/components/GroupOverview.jsx
+++ src/components/GroupOverview.jsx
@@ -1,28 +1,37 @@
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getGroup } from '../api/groups';
+
 import Icon from './Icon';
 import { NavLink } from 'react-router-dom';
-
 import { SlOptionsVertical } from 'react-icons/sl';
 
-const groupData = {
-  name: 'Movie Buffs',
-  description:
-    'Discuss your favorite films, discover new releases, and dive into movie reviews and critiques.',
-  overviewImage:
-    ' https://images.unsplash.com/photo-1526715875108-ed5fa46df641?q=80&w=1495&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-};
-
-const buttonsData = [
-  { name: 'Home', url: '/app/group/123' },
-  { name: 'Rooms', url: '/app/group/123/rooms' },
-  { name: 'About', url: '/app/group/123/about' },
-  { name: 'Members', url: '/app/group/123/members' },
-];
-
 export default function GroupOverview() {
+  const { groupId } = useParams();
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ['groups', groupId],
+    queryFn: () => getGroup(groupId),
+  });
+
+  if (isLoading) return 'Is loading...';
+  if (isError) return 'Error!';
+
+  const groupData = data.data.group;
+
+  const buttonsData = [
+    { name: 'Home', url: `/app/group/${groupId}` },
+    { name: 'Rooms', url: `/app/group/${groupId}/rooms` },
+    { name: 'About', url: `/app/group/${groupId}/about` },
+    { name: 'Members', url: `/app/group/${groupId}/members` },
+  ];
+
   return (
     <div className="rounded-2xl bg-base-100 shadow">
       <img
-        src={groupData.overviewImage}
+        src={
+          groupData.overviewImage ||
+          ' https://images.unsplash.com/photo-1526715875108-ed5fa46df641?q=80&w=1495&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        }
         className="h-52 w-full rounded-tl-2xl rounded-tr-2xl object-cover"
       />
       <div className="mt-8 flex justify-between p-4">
