812193e328738f3490b806c7ac261eb56582160b
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2024-09-16 16:09:18 -0300

    Add fetch to comment form

--- src/api/comments.js
+++ src/api/comments.js
@@ -3,3 +3,9 @@ import axiosInstance from './axiosInstance';
 export function getComments(id) {
   return axiosInstance.get(`/posts/${id}/comments`).then((res) => res.data);
 }
+
+export function createComment({ postId, content, author }) {
+  return axiosInstance
+    .post(`/posts/${postId}/comments`, { content, author })
+    .then((res) => res.data);
+}
--- src/pages/post/CreateComment.jsx
+++ src/pages/post/CreateComment.jsx
@@ -1,4 +1,32 @@
+import { useState } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import { createComment } from '../../api/comments';
+import { jwtDecode } from 'jwt-decode';
+
 export default function CreateComment() {
+  const [content, setContent] = useState('');
+
+  const queryClient = useQueryClient();
+  const { postId } = useParams();
+
+  const mutation = useMutation({
+    mutationFn: createComment,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['posts', postId, 'comments']);
+      setContent('');
+    },
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (content === '') return;
+
+    const author = jwtDecode(localStorage.getItem('jwt')).id;
+    mutation.mutate({ postId, content, author });
+  }
+
   return (
     <div className="flex gap-4">
       <img
@@ -6,16 +34,27 @@ export default function CreateComment() {
         alt="Profile picture."
         className="h-10 w-10 rounded-full"
       />
-      <form className="w-full" onClick={(e) => e.preventDefault()}>
+      <form className="w-full" onSubmit={handleSubmit}>
         <textarea
           className="textarea textarea-bordered w-full"
           placeholder="Write a comment"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
         ></textarea>
         <div className="flex justify-end gap-4">
-          <button type="button" className="btn mt-4">
+          <button
+            type="button"
+            className="btn mt-4"
+            disabled={mutation.isPending}
+            onClick={() => setContent('')}
+          >
             Clear
           </button>
-          <button type="submit" className="btn btn-primary mt-4">
+          <button
+            type="submit"
+            disabled={mutation.isPending}
+            className="btn btn-primary mt-4"
+          >
             Comment
           </button>
         </div>
