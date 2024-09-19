4b477184aba69f1b86ba7a9b2f24635fdbb91d10
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2024-09-18 22:40:12 -0300

    Add skeleton to feed

--- src/components/GroupOverview.jsx
+++ src/components/GroupOverview.jsx
@@ -69,7 +69,7 @@ export default function GroupOverview() {
           groupData.overviewImage ||
           ' https://images.unsplash.com/photo-1526715875108-ed5fa46df641?q=80&w=1495&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         }
-        className="h-52 w-full rounded-tl-2xl rounded-tr-2xl object-cover"
+        className="skeleton h-52 w-full rounded-tl-2xl rounded-tr-2xl object-cover"
       />
       <div className="mt-8 flex justify-between p-4">
         <div>
--- src/pages/group/GroupFeed.jsx
+++ src/pages/group/GroupFeed.jsx
@@ -4,6 +4,31 @@ import { getGroupPosts } from '../../api/groups';
 import PostPreview from './PostPreview';
 import useInfiniteScroll from './../../hooks/useInfiniteScroll';
 
+function LoadingSkeleton({ amount }) {
+  return (
+    <div className="space-y-6 rounded-2xl">
+      {Array.from({ length: amount }).map((_, index) => (
+        <div
+          key={index}
+          className="block rounded-2xl bg-base-100 px-4 py-4 shadow"
+        >
+          <div className="flex items-center gap-4">
+            <div className="skeleton h-10 w-10 rounded-full"></div>
+            <div className="skeleton h-6 w-20"></div>
+          </div>
+          <div className="mt-3">
+            <div className="skeleton h-6 w-full max-w-72"></div>
+            <div className="skeleton mt-2 h-5 w-full"></div>
+            <div className="skeleton mt-2 h-5 w-full"></div>
+            <div className="skeleton mt-2 h-5 w-full"></div>
+          </div>
+          <div className="skeleton mt-4 inline-block h-11 w-16 rounded-xl"></div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
 export default function GroupFeed() {
   const { groupId } = useParams();
   const {
@@ -26,7 +51,7 @@ export default function GroupFeed() {
 
   const loadMoreRef = useInfiniteScroll(hasNextPage, fetchNextPage);
 
-  if (isLoading) return <p>Loading...</p>;
+  if (isLoading) return <LoadingSkeleton amount={6} />;
   if (isError) return <p>Error!</p>;
 
   const posts = data.pages
@@ -43,6 +68,7 @@ export default function GroupFeed() {
       {posts.map((post) => {
         return <PostPreview data={post} key={post._id} />;
       })}
+      {isFetchingNextPage && <LoadingSkeleton amount={1} />}
       {hasNextPage && <div ref={loadMoreRef}></div>}
     </div>
   );
