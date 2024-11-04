2e40ddeb3b0c31bd438728b8bd0451dae822e47e
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2024-11-04 18:51:39 -0300

    Display group privacy in search page
    Display the privacy status of each group in the search results

--- src/pages/search/GroupCard.jsx
+++ src/pages/search/GroupCard.jsx
@@ -3,18 +3,30 @@ import LeaveButton from './LeaveButton';
 import JoinButton from './JoinButton';
 import { Link } from 'react-router-dom';
 
+import Icon from '../../components/Icon';
+import { FaLock } from 'react-icons/fa';
+
+import capitalizeFirstLetter from '../../utils/capitalize';
+
 export default function GroupCard({ data }) {
   return (
     <div className="rounded-xl bg-base-100 p-4 shadow">
       <div className="flex items-center gap-4">
-        {data.previewImage ? (
-          <img
-            src={data.previewImage}
-            className="skeleton aspect-square h-20 w-20 rounded-full object-cover"
-          />
-        ) : (
-          <GroupIcon icon={data.icon} />
-        )}
+        <div className="indicator">
+          {data.previewImage ? (
+            <img
+              src={data.previewImage}
+              className="skeleton aspect-square h-20 w-20 rounded-full object-cover"
+            />
+          ) : (
+            <GroupIcon icon={data.icon} />
+          )}
+          {data.private && (
+            <span className="badge indicator-item badge-secondary indicator-bottom left-7 top-12">
+              <Icon icon={<FaLock />} size="small" />
+            </span>
+          )}
+        </div>
         <div className="flex-1">
           <h2 className={`line-clamp-1 text-lg`}>
             {data.isJoined ? (
@@ -32,10 +44,14 @@ export default function GroupCard({ data }) {
             {data.description}
           </p>
         </div>
-        {!data.isJoined ? (
+        {data.requestStatus ? (
+          <button className="btn btn-disabled">
+            {capitalizeFirstLetter(data.requestStatus)}
+          </button>
+        ) : !data.isJoined ? (
           <JoinButton groupId={data._id} />
         ) : (
-          <LeaveButton groupId={data._id} />
+          data.isJoined && <LeaveButton groupId={data._id} />
         )}
       </div>
     </div>
