c28171418b0f63c5e6189a15b697e4a59d605a4f
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2024-11-06 18:20:57 -0300

    Update date format on Group About page

--- src/pages/about/PublicAbout.jsx
+++ src/pages/about/PublicAbout.jsx
@@ -3,6 +3,14 @@ import Icon from './../../components/Icon';
 import { HiPencil } from 'react-icons/hi2';
 
 export default function PublicAbout({ group }) {
+  const dateObject = new Date(group.createdAt);
+  const options = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  };
+  const createdAt = dateObject.toLocaleDateString('en-US', options);
+
   return (
     <div className="rounded-2xl bg-base-100 p-4 shadow">
       <div className="flex items-center justify-between">
@@ -16,7 +24,7 @@ export default function PublicAbout({ group }) {
         This group is <strong>{group.private ? 'Private' : 'Public'}</strong>.
       </p>
       <p className="mt-2">
-        <span className="font-bold">Created at:</span> {group.createdAt}
+        <span className="font-bold">Created at:</span> {createdAt}
       </p>
     </div>
   );
