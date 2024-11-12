9b33f7049957232811ec3f5f4ce6ac40b7ff4d2a
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2024-11-12 16:43:39 -0300

    Display user profile picture when editing a comment

--- src/pages/post/Comment.jsx
+++ src/pages/post/Comment.jsx
@@ -50,15 +50,13 @@ export default function Comment({ data }) {
           <p className="ml-14">{data.content}</p>
         </div>
       ) : (
-        <div>
-          <div className="flex items-center gap-4">
-            <img
-              src="https://images.unsplash.com/photo-1485846234645-a62644f84728?q=80&w=1459&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              className="skeleton h-10 w-10 rounded-full"
-            />
-            <div className="text-lg">{data.author.username}</div>
-          </div>
-          <form className="ml-14 w-full" onSubmit={handleSubmit}>
+        <div className="w-full">
+          <UserAvatar
+            url={data.author.profilePicture}
+            username={data.author.username}
+            color={data.author.placeholderColor}
+          />
+          <form className="w-full pl-14" onSubmit={handleSubmit}>
             <textarea
               className="textarea textarea-bordered w-full"
               placeholder="Write a comment"
