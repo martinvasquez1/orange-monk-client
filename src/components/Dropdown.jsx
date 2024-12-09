454262dcb17014eb8fb6ce0df81d653e8bcb8b34
Maycol Zincker <zincker2000@gmail.com>
2024-12-09 18:17:23 -0300

    Update dropdown shadow

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -6,7 +6,7 @@ export default function OptionsButton({ trigger, children }) {
       {trigger}
       <ul
         tabIndex={0}
-        className="menu dropdown-content z-[1] w-52 rounded-box bg-base-100 p-2 shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]"
+        className="menu dropdown-content z-[1] w-52 rounded-box bg-base-100 p-2 shadow-[0.0px_2.0px_10.0px_rgba(0,0,0,0.38)]"
       >
         {React.Children.map(children, (child) => (
           <li>{child}</li>
