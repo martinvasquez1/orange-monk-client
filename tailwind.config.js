0f15f91b7d8ba5452b1fd6231423012e37551cec
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2024-12-12 03:00:50 -0300

    Update Daisy import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -10,8 +12,8 @@ export default {
       },
     },
   },
-
-  plugins: [require('daisyui')],
+  //plugins: [require('daisyui')],
+  plugins: [daisyui],
   daisyui: {
     themes: [
       {
